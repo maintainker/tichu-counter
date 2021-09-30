4f9eef9d150003ad67331a9ac6bb5e77da663429
ssul <ssul@myrealplan.co.kr>
2021-10-01 08:17:04 +0900

    [feat] tmp

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 const playerA = ["김소라", "구민근"];
 const playerB = ["김태진", "정성우"];
 
-const resetBtn = document.querySelector("#header .reset");
+const init = ([playerA, playerB]) => {
+  const resetBtn = document.querySelector("#header .reset");
+  const userABox = document.querySelector("#userA");
+  const userBBox = document.querySelector("#userB");
+
+  //initializing
+  const initializing = () => {
+    const userA_1 = document.createElement("li");
+    const userA_2 = document.createElement("li");
+    const userB_1 = document.createElement("li");
+    const userB_2 = document.createElement("li");
+  };
+};
